6134c69d19ae36d65bf83f934d9e6aad48b2bf2c
Zzisan <zisaaannn@gmail.com>
2025-04-03 03:47:07 -0500

    Add products controller and views for index and show; update home page with product links.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all.order(:id)  # Optionally order products by id or any attribute.
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
