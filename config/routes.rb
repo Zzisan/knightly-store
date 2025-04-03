95324cdaaa7d23c5984465cd9f25d03f588038aa
Zzisan <zisaaannn@gmail.com>
2025-04-03 03:48:56 -0500

    updated console code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  get "products/index"
-  get "products/show"
   get "home/index"
   root 'home#index'
   
-    devise_for :admin_users, ActiveAdmin::Devise.config
-    ActiveAdmin.routes(self)
-    devise_for :customers
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :customers
   
-    resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show]
   
-    # Existing routes:
-    get "up" => "rails/health#show", as: :rails_health_check
-    get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  end
-  
+  # Existing routes:
+  get "up" => "rails/health#show", as: :rails_health_check
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+end
