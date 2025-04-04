57987bffd9772b5d10052f2a1f6be2b60a735a88
Zzisan <zisaaannn@gmail.com>
2025-04-04 02:26:47 -0500

    Integrate Active Storage for product images; update Product model, admin form, and product views.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,30 +1,36 @@
 ActiveAdmin.register Product do
-    permit_params :name, :description, :price, :stock_quantity, :image_url, :category_id
-  
-    index do
-      selectable_column
-      id_column
-      column :name
-      column "Category", :category
-      column :price
-      column :stock_quantity
-      actions
-    end
-  
-    filter :name
-    filter :category
-    filter :price
-  
-    form do |f|
-      f.inputs "Product Details" do
-        f.input :name
-        f.input :description
-        f.input :price
-        f.input :stock_quantity
-        f.input :image_url
-        f.input :category, as: :select, collection: Category.all.collect { |c| [c.name, c.id] }
+  permit_params :name, :description, :price, :stock_quantity, :image_url, :category_id, :image
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column "Category", :category
+    column :price
+    column :stock_quantity
+    column "Image" do |product|
+      if product.image.attached?
+        image_tag url_for(product.image), width: "50"
+      else
+        "No image"
       end
-      f.actions
     end
+    actions
   end
-  
+
+  filter :name
+  filter :category
+  filter :price
+
+  form do |f|
+    f.inputs "Product Details" do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :stock_quantity
+      f.input :category, as: :select, collection: Category.all.collect { |c| [c.name, c.id] }
+      f.input :image, as: :file  # This adds the file upload field
+    end
+    f.actions
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,8 @@ class Product < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
+  # Add this line to attach an image
+  has_one_attached :image
+
   validates :name, :description, :price, :stock_quantity, presence: true
 end
