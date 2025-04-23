c2208bb86e16bb66fce54184069a5042a02d5468
Zisan <zisaaannn@gmail.com>
2025-04-22 23:36:14 -0500

    Set up Province, hook Customer→Province, update Order model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,8 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :orders, dependent: :destroy
-
+  belongs_to :province, optional: true
+  
   # Ransack: Allowlist searchable attributes
   def self.ransackable_attributes(auth_object = nil)
     ["id", "email", "first_name", "last_name", "phone", "address", "created_at", "updated_at"]
